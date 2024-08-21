b5554af4d244ae8bbef95262e6b0df21497255c0
Emmamartin1185 <emv.martin@alumno.um.edu.ar>
2024-08-21 16:29:52 -0300

    pawn

--- ajedrez/pawn.py
+++ ajedrez/pawn.py
@@ -1,6 +1,37 @@
-from piece import Piece
+class Pawn:
+    def __init__(self, color):
+        self.__color__ = color
+        self.__symbol__ = "♙" if color == "WHITE" else "♟"
+        self.__moved__ = False
 
+    def __str__(self):
+        return self.__symbol__
 
-class Pawn(Piece):
-    ...
+    def valid_moves(self, position, board):
+        """
+        Retorna una lista de posiciones válidas para mover el peón.
+        El movimiento depende del color y si el peón ya se ha movido.
+        """
+        moves = []
+        row, col = position
+
+        # Movimiento para peones blancos (hacia arriba)
+        if self.__color__ == "WHITE":
+            if board.get_piece(row - 1, col) is None:  # Movimiento simple hacia adelante
+                moves.append((row - 1, col))
+                if not self.__moved__ and board.get_piece(row - 2, col) is None:  # Movimiento doble inicial
+                    moves.append((row - 2, col))
+
+        # Movimiento para peones negros (hacia abajo)
+        elif self.__color__ == "BLACK":
+            if board.get_piece(row + 1, col) is None:  # Movimiento simple hacia adelante
+                moves.append((row + 1, col))
+                if not self.__moved__ and board.get_piece(row + 2, col) is None:  # Movimiento doble inicial
+                    moves.append((row + 2, col))
+
+        return moves
+
+    def move(self):
+        self.__moved__ = True
+
  
--- changelog.md
+++ changelog.md
@@ -1,10 +1,16 @@
-## [1.1.1] - 19-08-2024
+# - 21-08-2024
+- Se creo el archivo pawn.py con la funcion init,str ,valid_moves y move
+- Se creo el test de pawn
+# - 20-08-2024
+- Se creo el test de board
+-Se arreglo el git ignore 
+# - 19-08-2024
 
 ### Added
 - Se creo el archivo board con la funcion init y get_piece
 - Se creo el archivo chess con la funcion init, move y change turn
 - Se creo el archivo cly con la funcion main y play
-- Se creo el archivo pawn con la funcion
-- Se creo el archivo rook con la funcion
+- Se creo el archivo pawn 
+- Se creo el archivo rook 
 - Se creo el archivo piece con la funcion init 
 
